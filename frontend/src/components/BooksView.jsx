69d302b0a477e304ece9328bcbe3d46bd605121f
JoshuaBarawa <mwamlandabarawa@gmail.com>
2024-06-10 13:14:43 +0300

    added load more button

--- frontend/src/components/BooksView.jsx
+++ frontend/src/components/BooksView.jsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react'
-import './booksview.css'
+import React, { useEffect, useState } from 'react';
+import './booksview.css';
 
 import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
@@ -12,21 +12,19 @@ import ListItemText from '@mui/material/ListItemText';
 import { useQuery } from "@apollo/client";
 import { GET_BOOKS } from "../graphql/Queries";
 
-
 const BooksView = () => {
-    const { data } = useQuery(GET_BOOKS);
+    const { data, error } = useQuery(GET_BOOKS);
     
-    const [allBooks, setAllbooks] = useState([])
+    const [allBooks, setAllBooks] = useState([]);
     const [searchTitle, setSearchTitle] = useState('');
     const [filteredBooks, setFilteredBooks] = useState([]);
-    const [displayedBooksCount, setDisplayedBooksCount] = useState(10);
+    const [displayedBooksCount, setDisplayedBooksCount] = useState(12);
   
-    useEffect(()=>{
+    useEffect(() => {
         if (data) {
-            setAllbooks(data?.books)
+            setAllBooks(data?.books);
         }
-    }, [data])
-
+    }, [data]);
 
     useEffect(() => {
         if (searchTitle.trim() === '') {
@@ -39,79 +37,84 @@ const BooksView = () => {
         }
     }, [searchTitle, allBooks]);
 
-
     const handleSearchInputChange = (e) => {
         setSearchTitle(e.target.value);
     };
 
     const handleAddNewBook = (book) => {
-        setSearchTitle('')
-        setAllbooks([book, ...allBooks])
+        setSearchTitle('');
+        setAllBooks([book, ...allBooks]);
     };
 
     const handleLoadMoreBooks = () => {
-        setDisplayedBooksCount(prevCount => prevCount + 10);
+        setDisplayedBooksCount(prevCount => prevCount + 12);
     };
 
     const Item = styled(Paper)(({ theme }) => ({
         padding: theme.spacing(1),
         color: theme.palette.text.secondary,
         height: '200px',
-      }));
+    }));
+
+    if (error) {
+        return <p>Error fetching books: {error.message}</p>;
+    }
 
     return (
-    <div className='main-container'>
-        
-    <h1 className='container-title'><span>Tiny Tales:</span> Books for Little Dreamers</h1>
-
-    <div className='search-book-section'>
-            <input type='text' className="search-book-input" value={searchTitle} onChange={handleSearchInputChange}
-              placeholder='Find book i.e title' />
-            {searchTitle?
-            <Grid item xs={14} md={6} className='search-list-section'>
-                <List dense={true}>
-                {filteredBooks.length > 0?
-                    filteredBooks.map((book, index) => 
-                        <ListItem secondaryAction={
-                            <button className='add-book-btn' onClick={()=>handleAddNewBook(book)}>Add book</button>
-                        } key={index} className='book-item'>
-                        <ListItemText primary={book.title}
-                            secondary={`by ${book.author}`}/>
-                        </ListItem>,
-                    ) : <p id='not-found-info'>Book with title "{searchTitle}" not found!</p>
-            } 
-                </List>
-            </Grid> : ''
-            }
-    </div>
-
-         
-    <Box sx={{ width: '100%', paddingBottom: '50px' }}>
-            <Grid container spacing={1}>
-                {allBooks.slice(0, displayedBooksCount).map((book, index) => 
-                    <Grid item xs={12} sm={6} md={4} lg={2} xl={2} key={index}>
-                        <Item>
-                            <img src={`../${book?.coverPhotoURL}`} alt="book-cover" id='book-cover'/>
-                            <p id='book-title'>{book?.title}</p>
-                            <div className='book-author-level'>
-                                <p id="book-author">by {book?.author}</p>
-                                <p id="book-author">level: {book?.readingLevel}</p>
-                            </div>
-                        </Item>
+        <div className='main-container'>
+            <h1 className='container-title'><span>Tiny Tales:</span> Books for Little Dreamers</h1>
+            <div className='search-book-section'>
+                <input
+                    type='text'
+                    className="search-book-input"
+                    value={searchTitle}
+                    onChange={handleSearchInputChange}
+                    placeholder='Find book i.e title'
+                />
+                {searchTitle && (
+                    <Grid item xs={14} md={6} className='search-list-section'>
+                        <List dense={true}>
+                            {filteredBooks.length > 0 ?
+                                filteredBooks.map((book, index) => (
+                                    <ListItem
+                                        secondaryAction={
+                                            <button className='add-book-btn' onClick={() => handleAddNewBook(book)}>Add book</button>
+                                        }
+                                        key={index}
+                                        className='book-item'
+                                    >
+                                        <ListItemText primary={book.title} secondary={`by ${book.author}`} />
+                                    </ListItem>
+                                )) :
+                                <p id='not-found-info'>Book with title "{searchTitle}" not found!</p>
+                            }
+                        </List>
                     </Grid>
                 )}
-            </Grid>
-            {displayedBooksCount < allBooks.length && (
-                <div className='load-more-section'>
-                    <button className='load-more-btn' onClick={handleLoadMoreBooks}>Load More</button>
-                </div>
-            )}
-        </Box>
-
-
+            </div>
+            <Box sx={{ width: '100%', paddingBottom: '50px' }}>
+                <Grid container spacing={1}>
+                    {allBooks.slice(0, displayedBooksCount).map((book, index) => (
+                        <Grid item xs={12} sm={6} md={4} lg={2} xl={2} key={index}>
+                            <Item>
+                                <img src={`../${book?.coverPhotoURL}`} alt="book-cover" id='book-cover' />
+                                <p id='book-title'>{book?.title}</p>
+                                <div className='book-author-level'>
+                                    <p id="book-author">by {book?.author}</p>
+                                    <p id="book-author">level: {book?.readingLevel}</p>
+                                </div>
+                            </Item>
+                        </Grid>
+                    ))}
+                </Grid>
+                {displayedBooksCount < allBooks.length && (
+                    <div className='load-more-section'>
+                        <button className='load-more-btn' onClick={handleLoadMoreBooks}>Load More</button>
+                    </div>
+                )}
+            </Box>
         </div>
-    )
+    );
+};
 
-
-}
-export default BooksView
+export default BooksView;
